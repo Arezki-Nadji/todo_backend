59375f43da0360fbc05a40449e64636c63fc8d66
Arezki-Nadji <arezki.m.nadji96@gmail.com>
2023-07-30 14:13:30 +0200

    added serialize for Todo and TodoList

--- todo_backend/todo/views.py
+++ todo_backend/todo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from todo.models import Todo, TodoList
+from todo.serializers import TodoListSerializer, TodoSerializer
 
-# Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated, )
+
+class TodoListViewSet(viewsets.ModelViewSet):
+
+    queryset = TodoList.objects.all()
+    serializer_class = TodoListSerializer
+    permission_classes = (IsAuthenticated, )
