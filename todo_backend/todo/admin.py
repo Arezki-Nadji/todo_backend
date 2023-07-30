7ffdc4ef41e092e08f115e3f85876ef78debe3b7
Arezki-Nadji <arezki.m.nadji96@gmail.com>
2023-07-30 13:21:43 +0200

    added Todo and TodoList model also added to admin.py

--- todo_backend/todo/admin.py
+++ todo_backend/todo/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Todo, TodoList
 
-# Register your models here.
+class TodoInline(admin.TabularInline):
+
+    model = Todo
+    extra = 0
+
+@admin.register(TodoList)
+class TodoListAdmin(admin.ModelAdmin):
+
+    list_display = ('name', )
+    inlines = (TodoInline, )
+   
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    
+    list_display = ('title','due_date', 'completed', 'favourite')
+    list_filter = ('due_date', 'completed', 'favourite')
+    search_fields = ('title', )
--- todo_backend/todo/models.py
+++ todo_backend/todo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+
+    title = models.CharField(max_length=255)
+    due_date = models.DateField()
+    completed = models.BooleanField()
+    favourite = models.BooleanField()
+    list = models.ForeignKey('TodoList',null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class TodoList(models.Model):
+
+    name = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = 'Todo List'
+        verbose_name_plural = 'Todo Lists'  
+          
+    def __str__(self):
+        return self.name
+
